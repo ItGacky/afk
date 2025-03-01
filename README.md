cd278a38242eebb312e1843e191ca5d3b5868486
ItGacky <itagaki.takahiro@gmail.com>
2025-03-01 18:34:58 +0900

    Added "auto" option.

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ GNU General Public License, version 3 (GPL-3.0)
 ## Usage
 - `afk.exe sleep` to suspend monitor.
 - `afk.exe suspend` to suspend PC, unless you are downloading files.
+- `afk.exe auto` to suspend PC, or suspend monitor if you are downloading files.
--- afk.cpp
+++ afk.cpp
@@ -35,23 +35,58 @@ static bool isDownloading() {
 	return exists;
 }
 
+enum ModeID {
+	SLEEP,
+	SUSPEND,
+	AUTO
+};
+
+const struct Mode {
+	ModeID			id;
+	const WCHAR*	name;
+} MODES[] = {
+	{ AUTO, L"auto" },
+	{ SLEEP, L"sleep" },
+	{ SUSPEND, L"suspend" }
+};
+
+static const Mode* parseArgs(const WCHAR* args) {
+	for (auto& mode : MODES) {
+		if (0 == wcscmp(args, mode.name))
+			return &mode;
+	}
+	return nullptr;
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, WCHAR* args, int show) {
-	if (0 == wcscmp(args, L"sleep")) {
-		PostMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
-	} else if (0 == wcscmp(args, L"suspend")) {
-		if (!isDownloading()) {
+	if (auto mode = parseArgs(args)) {
+		// suspend PC
+		switch (mode->id) {
+		case AUTO:
+		case SUSPEND:
+			if (isDownloading())
+				break;
 			if (auto powrprof = LoadLibraryW(L"powrprof.dll")) {
 				if (auto SetSuspendState = reinterpret_cast<BOOL(WINAPI*)(BOOL, BOOL, BOOL)>(GetProcAddress(powrprof, "SetSuspendState"))) {
 					SetSuspendState(FALSE, FALSE, FALSE);
 				}
 				FreeLibrary(powrprof);
 			}
+			return 0;
 		}
-		else {
-			alert(hInstance, IDS_CANCEL_SUSPEND, MB_OK | MB_ICONWARNING);
+		// suspend monitor
+		switch (mode->id) {
+		case AUTO:
+		case SLEEP:
+			PostMessageW(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
+			return 0;
 		}
+		// canceled
+		alert(hInstance, IDS_CANCEL_SUSPEND, MB_OK | MB_ICONWARNING);
+		return 2;
 	} else {
+		// bad usage
 		alert(hInstance, IDS_USAGE, MB_OK | MB_ICONERROR);
+		return 1;
 	}
-	return 0;
 }
